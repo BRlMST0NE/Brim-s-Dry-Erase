7d095f9f25200ac8e7077b5d19e2227d50cf3959
BRlMST0NE <151819644+BRlMST0NE@users.noreply.github.com>
2024-03-02 16:45:52 -0800

    Create ᏼꭱꮖꮇꮪꭲσɴꭼ's Dry Erase.js

---
+++ ᏼꭱꮖꮇꮪꭲσɴꭼ's Dry Erase.js
@@ -0,0 +1,22 @@
+// ==UserScript==
+// @name         ᏼꭱꮖꮇꮪꭲσɴꭼ's Dry Erase
+// @namespace    http://tampermonkey.net/
+// @version      0.0.1
+// @description  "Fixes" hit marker caching issues.
+// @match        *://shellshock.io/*
+// @grant        none
+// @run-at       document-start
+// ==/UserScript==
+
+window.XMLHttpRequest = class extends window.XMLHttpRequest {
+	open( method, url ) {
+		if ( url.indexOf( 'shellshock.js' ) > - 1 ) {
+			this.isScript = true;
+		}   return super.open( ...arguments );
+	}
+	get response() {
+		if ( this.isScript ) {
+			return super.response.replace_all(`if(0===this.container.visibility)`,`getStoredBool("hitMarkers",!0)?this.show():this.hide();if(0===this.container.visibility)`)
+		}   return super.response;
+	}
+};
